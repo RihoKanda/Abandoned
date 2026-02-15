8198281835c8b4c2933cc559ad48c3ba7dc9af4d
vahkd33114 <vahkd33114@yahoo.co.jp>
2026-02-16 06:03:11 +0900

    update: BattleUIController.cs 更新

--- Assets/Scripts/BattleUIController.cs
+++ Assets/Scripts/BattleUIController.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Abandoned.Battle;
 
-public class BattleUIController : MonoBehaviour
+namespace Abandoned.UI
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// バトルシーン　UI表示　管理
+    /// </summary>
+    public class BattleUIController : MonoBehaviour
     {
-        
-    }
+        [Header("バトル情報表示")] 
+        [SerializeField] private TextMeshProUGUI battleLogText;
+        [SerializeField] private TextMeshProUGUI enemyCountText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Update()
+        {
+            UpdateBattleInfo();
+        }
+
+        private void UpdateBattleInfo()
+        {
+            if (BattleManager.Instance == null) return;
+
+            var enemy = BattleManager.Instance.CurrentEnemy;
+            if (enemy != null && enemyCountText != null)
+            {
+                enemyCountText.text = $"現在の敵: {enemy.name}";
+            }
+        }
     }
 }
